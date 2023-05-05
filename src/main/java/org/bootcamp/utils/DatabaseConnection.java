26f181eda41edd39c41adeeb4e5e7201c98b332a
Israel Jasma <israel.jasma@gmail.com>
2023-05-04 20:37:00 -0400

    Feat: Conexion base de datos

--- src/main/java/org/bootcamp/utils/DatabaseConnection.java
+++ src/main/java/org/bootcamp/utils/DatabaseConnection.java
@@ -1,2 +1,15 @@
-package org.bootcamp.utils;public class DatabaseConnection {
+package org.bootcamp.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static String url = "jbdc:mysql://localhost:3306/javaweb?serverTimezone=America/Santiago";
+    private static String username = "root";
+    private static String password = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
+    }
 }
