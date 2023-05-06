4964117db4460f6f6bbc7e8fce9fa31e64c440b8
Israel Jasma <israel.jasma@gmail.com>
2023-05-06 13:36:05 -0400

    Feat: Listar capacitacion servlet, doGet

---
+++ src/main/java/org/bootcamp/controllers/ListarCapacitacionesServlet.java
@@ -0,0 +1,32 @@
+package org.bootcamp.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.bootcamp.models.Capacitacion;
+import org.bootcamp.repositories.CapacitacionRepositoryJdbcImpl;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet("/listarcapacitaciones")
+public class ListarCapacitacionesServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        CapacitacionRepositoryJdbcImpl repository = new CapacitacionRepositoryJdbcImpl();
+        List<Capacitacion> capacitaciones;
+
+
+        try {
+            capacitaciones = repository.listar();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        req.setAttribute("capacitaciones", capacitaciones);
+        getServletContext().getRequestDispatcher("/listarcapacitaciones.jsp").forward(req,resp);
+    }
+}
