599b1e0a7697d94b62ae73c17e4a4c34306e189d
Israel Jasma <israel.jasma@gmail.com>
2023-05-06 10:31:30 -0400

    Feat: guardar formulario de contacto en bd

--- src/main/java/org/bootcamp/controllers/ContactoServlet.java
+++ src/main/java/org/bootcamp/controllers/ContactoServlet.java
@@ -5,9 +5,12 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.bootcamp.repositories.ContactoRepositoryJdbcImpl;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @WebServlet("/contacto")
 public class ContactoServlet extends HttpServlet {
@@ -18,24 +21,42 @@ public class ContactoServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ContactoRepositoryJdbcImpl contactoRepository = new ContactoRepositoryJdbcImpl();
         String nombre = req.getParameter("nombre");
         String telefono = req.getParameter("telefono");
         String email = req.getParameter("email");
         String mensaje = req.getParameter("mensaje");
 
+        int result;
+
+        try {
+            result = contactoRepository.createContacto(nombre, Integer.parseInt(telefono), email, mensaje);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
         PrintWriter out = resp.getWriter();
-
-        // Escribir en la respuesta HTTP al navegador
         out.println("<html><body>");
-        out.println("<h1>" + nombre + "</h1>");
-        out.println("<h1>" + telefono + "</h1>");
-        out.println("<h1>" + email + "</h1>");
-        out.println("<h1>" + mensaje + "</h1>");
-        // Escribir en la consola del navegador usando JavaScript
-        out.println("<script>");
-        out.println("console.log('Hola desde MiServlet!');");
-        out.println("</script>");
+        if (result != 0) {
+            // Escribir en la respuesta HTTP al navegador
+            out.println("<h1>" + nombre + "</h1>");
+            out.println("<h1>" + telefono + "</h1>");
+            out.println("<h1>" + email + "</h1>");
+            out.println("<h1>" + mensaje + "</h1>");
+            // Escribir en la consola del navegador usando JavaScript
+            out.println("<script>");
+            out.println("console.log('Hola desde MiServlet!');");
+            out.println("</script>");
 
-        out.println("</body></html>");
+            out.println("</body></html>");
+        }else {
+            out.println("<h1>Ocurrio un error al crear el formulario de contacto</h1>");
+            out.println("<script>");
+            out.println("console.log('Hola desde MiServlet!');");
+            out.println("</script>");
+
+            out.println("</body></html>");
+        }
     }
 }
