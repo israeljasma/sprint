5e45a5b32c1f900b3480d34a61ae07ea62ca5901
Israel Jasma <israel.jasma@gmail.com>
2023-05-06 02:34:33 -0400

    Feat: Servlet listar usuarios

---
+++ src/main/java/org/bootcamp/controllers/ListarUsuarios.java
@@ -0,0 +1,32 @@
+package org.bootcamp.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.bootcamp.models.Usuario;
+import org.bootcamp.repositories.UsuarioRepositoryJdbcImpl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet("/listarusuarios")
+public class ListarUsuarios extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UsuarioRepositoryJdbcImpl repository = new UsuarioRepositoryJdbcImpl();
+        List<Usuario> usuarios =  null;
+
+        try {
+            usuarios = repository.listar();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        req.setAttribute("usuarios", usuarios);
+        getServletContext().getRequestDispatcher("/listarusuarios.jsp").forward(req, resp);
+    }
+}
