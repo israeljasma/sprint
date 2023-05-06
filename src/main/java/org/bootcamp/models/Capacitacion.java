9a1de9a6cb3e6a4259bbddb067e8d502b6f705f6
Israel Jasma <israel.jasma@gmail.com>
2023-05-06 11:54:04 -0400

    Feat+Fix: getters y setter, arreglo en tipo de datos

--- src/main/java/org/bootcamp/models/Capacitacion.java
+++ src/main/java/org/bootcamp/models/Capacitacion.java
@@ -1,10 +1,13 @@
 package org.bootcamp.models;
 
+import java.time.LocalTime;
+import java.util.Date;
+
 public class Capacitacion {
-    private String id;
+    private int id;
     private String rutCliente;
-    private String dia;
-    private String hora;
+    private Date dia;
+    private LocalTime hora;
     private String lugar;
     private int duracion;
     private int cantidad;
@@ -12,7 +15,7 @@ public class Capacitacion {
     public Capacitacion() {
     }
 
-    public Capacitacion(String rutCliente, String dia, String hora, String lugar, int duracion, int cantidad) {
+    public Capacitacion(String rutCliente, Date dia, LocalTime hora, String lugar, int duracion, int cantidad) {
         this.rutCliente = rutCliente;
         this.dia = dia;
         this.hora = hora;
@@ -21,13 +24,69 @@ public class Capacitacion {
         this.cantidad = cantidad;
     }
 
-    public Capacitacion(String id, String rutCliente, String dia, String hora, String lugar, int duracion, int cantidad) {
+    public Capacitacion(int id, String rutCliente, Date dia, LocalTime hora, String lugar, int duracion, int cantidad) {
         this.id = id;
         this.rutCliente = rutCliente;
         this.dia = dia;
         this.hora = hora;
         this.lugar = lugar;
         this.duracion = duracion;
+        this.cantidad = cantidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRutCliente() {
+        return rutCliente;
+    }
+
+    public void setRutCliente(String rutCliente) {
+        this.rutCliente = rutCliente;
+    }
+
+    public Date getDia() {
+        return dia;
+    }
+
+    public void setDia(Date dia) {
+        this.dia = dia;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
     }
 }
