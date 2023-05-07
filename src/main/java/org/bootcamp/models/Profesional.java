4f9e86a50515d28c93b66aba4a3075da3dde451f
Israel Jasma <israel.jasma@gmail.com>
2023-05-06 23:09:07 -0400

    Feat: Model profesional

---
+++ src/main/java/org/bootcamp/models/Profesional.java
@@ -0,0 +1,40 @@
+package org.bootcamp.models;
+
+import java.util.Date;
+
+public class Profesional extends Usuario {
+    private String titulo;
+    private String fechaIngreso;
+
+    public Profesional() {
+        super();
+    }
+
+    public Profesional(String username, String nombres, String apellidos, Date fechaNacimiento, String rut, String password, String titulo, String fechaIngreso) {
+        super(username, nombres, apellidos, fechaNacimiento, rut, password);
+        this.titulo = titulo;
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public Profesional(int id, String username, String nombres, String apellidos, Date fechaNacimiento, String rut, String password, String titulo, String fechaIngreso) {
+        super(id, username, nombres, apellidos, fechaNacimiento, rut, password);
+        this.titulo = titulo;
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(String fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+}
