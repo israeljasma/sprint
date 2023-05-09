6f8f23039cfb8d8514171f67f9be793b58ac295e
Israel Jasma <israel.jasma@gmail.com>
2023-05-09 02:59:53 -0400

    Feat: atributos faltastes agragdos

--- src/main/java/org/bootcamp/repositories/UsuarioRepositoryJdbcImpl.java
+++ src/main/java/org/bootcamp/repositories/UsuarioRepositoryJdbcImpl.java
@@ -28,6 +28,11 @@ public class UsuarioRepositoryJdbcImpl implements Repository{
                 usuario.setId(resultSet.getInt("id"));
                 usuario.setUsername(resultSet.getString("username"));
                 usuario.setPassword(resultSet.getString("password"));
+                usuario.setNombres(resultSet.getString("nombres"));
+                usuario.setApellidos(resultSet.getString("apellidos"));
+                usuario.setFechaNacimiento(resultSet.getDate("fecha_nacimiento"));
+                usuario.setRut(resultSet.getString("rut"));
+                usuario.setRol(resultSet.getInt("rol"));
                 return usuario;
             } else {
                 return null;
