0e49fa55b5bd7f3484aaa2172e82bef3a78aca6d
Israel Jasma <israel.jasma@gmail.com>
2023-05-09 12:25:57 -0400

    Feat: Modelo rol

---
+++ src/main/java/org/bootcamp/models/Rol.java
@@ -0,0 +1,34 @@
+package org.bootcamp.models;
+
+public class Rol {
+    private int id;
+    private String nombre;
+
+    public Rol() {
+    }
+
+    public Rol(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Rol(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
