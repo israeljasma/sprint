9c3650e74d6b7c7ceeda6c8027f288e30563cf68
Israel Jasma <israel.jasma@gmail.com>
2023-05-09 12:26:39 -0400

    Fix: logout corregido de string a usuario

--- src/main/java/org/bootcamp/controllers/LogoutServlet.java
+++ src/main/java/org/bootcamp/controllers/LogoutServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.bootcamp.models.Usuario;
 import org.bootcamp.services.LoginService;
 import org.bootcamp.services.LoginServiceImpl;
 
@@ -16,11 +17,10 @@ import java.util.Optional;
 public class LogoutServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         LoginService auth = new LoginServiceImpl();
-        Optional<String> username = auth.getUsername(req);
-        if (username.isPresent()){
+        Optional<Usuario> user = auth.getUsername(req);
+        if (user.isPresent()){
             HttpSession session = req.getSession();
             session.invalidate();
-
         }
         resp.sendRedirect(req.getContextPath() + "/login");
     }
