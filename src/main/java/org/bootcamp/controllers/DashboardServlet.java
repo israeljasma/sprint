43b85aff0f429ef9d0f433ea3c4708fa749aaeed
Israel Jasma <israel.jasma@gmail.com>
2023-05-10 03:29:55 -0400

    Style: cambio en nombre

--- src/main/java/org/bootcamp/controllers/DashboardServlet.java
+++ src/main/java/org/bootcamp/controllers/DashboardServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/dashboard")
-public class dashboard extends HttpServlet {
+public class DashboardServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.sendRedirect(req.getContextPath() + "/dashboard.jsp");
