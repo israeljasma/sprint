5d50cfc25f588913e770905dae439fe313d4310a
Israel Jasma <israel.jasma@gmail.com>
2023-05-10 21:26:07 -0400

    Feat: Ruta eliminar capacitaciones progetida

--- src/main/java/org/bootcamp/controllers/CapacitacionesEliminarServlet.java
+++ src/main/java/org/bootcamp/controllers/CapacitacionesEliminarServlet.java
@@ -5,17 +5,66 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.bootcamp.models.Rol;
+import org.bootcamp.models.Usuario;
 import org.bootcamp.repositories.CapacitacionRepositoryJdbcImpl;
+import org.bootcamp.repositories.RolRepositoryJdbcImpl;
+import org.bootcamp.services.LoginService;
+import org.bootcamp.services.LoginServiceImpl;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
 @WebServlet("/capacitaciones/eliminar")
 public class CapacitacionesEliminarServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LoginService auth = new LoginServiceImpl();
         CapacitacionRepositoryJdbcImpl repository = new CapacitacionRepositoryJdbcImpl();
+        RolRepositoryJdbcImpl rolRepository = new RolRepositoryJdbcImpl();
+        Optional<Usuario> userOptional = auth.getUser(req);
+        List<Rol> roles;
         int id;
+
+        try {
+            roles = rolRepository.listar();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (userOptional.isPresent()){
+            Rol rol = null;
+            for (Rol getRol: roles){
+                if (getRol.getId() == userOptional.get().getRol()){
+                    rol = new Rol();
+                    rol.setId(getRol.getId());
+                    rol.setNombre(getRol.getNombre());
+                    break;
+                }
+            }
+            if (rol.getNombre().equals("administrador")){
+                try {
+                    id = Integer.parseInt(req.getParameter("id"));
+                }catch (NumberFormatException e){
+                    id = 0;
+                }
+                if (id > 0 ){
+                    try {
+                        repository.delete(id);
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                resp.sendRedirect(req.getContextPath()+ "/capacitaciones");
+            } else {
+                resp.sendRedirect(req.getContextPath() + "/login");
+            }
+            resp.sendRedirect(req.getContextPath() + "/login");
+        }
+
+
 
         try {
             id = Integer.parseInt(req.getParameter("id"));
