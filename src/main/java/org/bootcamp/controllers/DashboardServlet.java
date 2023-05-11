bf089803a80994b649e5ac034188fd49f76f5e02
Israel Jasma <israel.jasma@gmail.com>
2023-05-11 12:29:31 -0400

    Feat: proteccion de ruta

--- src/main/java/org/bootcamp/controllers/DashboardServlet.java
+++ src/main/java/org/bootcamp/controllers/DashboardServlet.java
@@ -5,13 +5,49 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.bootcamp.models.Rol;
+import org.bootcamp.models.Usuario;
+import org.bootcamp.repositories.RolRepositoryJdbcImpl;
+import org.bootcamp.services.LoginService;
+import org.bootcamp.services.LoginServiceImpl;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
 @WebServlet("/dashboard")
 public class DashboardServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.sendRedirect(req.getContextPath() + "/dashboard.jsp");
+        LoginService auth = new LoginServiceImpl();
+        RolRepositoryJdbcImpl rolRepository = new RolRepositoryJdbcImpl();
+        Optional<Usuario> userOptional = auth.getUser(req);
+        List<Rol> roles;
+
+        try {
+            roles = rolRepository.listar();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (userOptional.isPresent()){
+            Rol rol = null;
+            for (Rol getRol: roles){
+                if (getRol.getId() == userOptional.get().getRol()){
+                    rol = new Rol();
+                    rol.setId(getRol.getId());
+                    rol.setNombre(getRol.getNombre());
+                    break;
+                }
+            }
+            if (rol.getNombre().equals("administrador")){
+                getServletContext().getRequestDispatcher("/WEB-INF/dashboard.jsp").forward(req, resp);
+            }else {
+                resp.sendRedirect(req.getContextPath() + "/contacto");
+            }
+        }else {
+            resp.sendRedirect(req.getContextPath() + "/login");
+        }
     }
 }
